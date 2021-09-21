da01034c4f4affe80362eaf8c9e4d4151773f37c
BuildTools <unconfigured@null.spigotmc.org>
2021-09-21 09:56:20 -0300

    agregar menu lateral 2 jaja

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,12 +2,13 @@ import { EncabezadoComponent } from './encabezado/encabezado.component';
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [EncabezadoComponent],
-  exports:[EncabezadoComponent],
+  declarations: [EncabezadoComponent,FooterComponent],
+  exports:[EncabezadoComponent,FooterComponent],
   imports: [
     IonicModule,
     CommonModule
